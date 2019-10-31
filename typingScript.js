552fb2a18b9f0799fa42d7b3537268d957bfa8e9
lukemuehring <muehring.luke@gmail.com>
2019-10-30 23:58:17 -0400

    added realtime text changes, less efficient but more responsive. Can also copy and paste blocks of text in now

--- typingScript.js
+++ typingScript.js
@@ -32,26 +32,8 @@ var emojiArr = [
             0x1F4B9, //Y
             0x1F4A4, //Z
            ];
+var outputString;
 
-function toUTF16(codePoint) {
-  var TEN_BITS = parseInt('1111111111', 2);
-  function u(codeUnit) {
-    return '\\u'+codeUnit.toString(16).toUpperCase();
-  }
-
-  if (codePoint <= 0xFFFF) {
-    return u(codePoint);
-  }
-  codePoint -= 0x10000;
-
-  // Shift right to get to most significant 10 bits
-  var leadSurrogate = 0xD800 + (codePoint >> 10);
-
-  // Mask to get least significant 10 bits
-  var tailSurrogate = 0xDC00 + (codePoint & TEN_BITS);
-
-  return u(leadSurrogate) + u(tailSurrogate);
-}
 function fancyCount2(str){
   const joiner = "\u{200D}";
   const split = str.split(joiner);
@@ -67,6 +49,15 @@ function fancyCount2(str){
   return count / split.length;
 }
 
+//a function that takes in the unicode code point of a character, and returns the emoji at the corresponding array index if it's within bounds (a-z). If not within bounds, it returns -1.
+function textToEmoji(currentCode) {
+    if ((currentCode - 97) >= 0 && (currentCode - 97) <= 26) {
+        return emojiArr[currentCode - 97];
+    } else {
+        return -1;
+    }
+}
+
 //! Need to figure out how to replace keypress with keydown.
 function updateInput(e, current) {
     //e is the event object
@@ -75,39 +66,69 @@ function updateInput(e, current) {
     //@@@ Take into account multiple word special cases like OK, 1234, ATM, GO and UP.
     //@@@ Be able to delete spaces
     //@@@ Alternate emojis by using a modifier key like SHIFT
-    
-    var thisKey = e.key;
-    console.log(thisKey);
-    currentCode = thisKey.charCodeAt(0);
-    console.log("charCode "+ currentCode);
-    if ((currentCode - 97) >= 0 && (currentCode - 97) <= 26) {
-        var updatedValue = emojiArr[currentCode - 97]; //-65 so that A is indexing at 0
-        outputText.innerHTML = outputText.innerHTML.concat(String.fromCodePoint(updatedValue));  
-    } else if (thisKey == "Backspace") {
-        var currentText = outputText.innerText;
-//        var lastChar = currentText.charAt(currentText.length - 1);
-//        var lengthOfLastChar = fancyCount2(lastChar);
-//        console.log("lastChar " + lastChar);
-//        console.log("length of the char " + lengthOfLastChar);
-        if (currentText.length > 4) {
-            if (currentText.substring(currentText.length-6) == "&nbsp;") {
-                outputText.innerHTML = outputText.innerHTML.substring(0, outputText.innerHTML.length - 7);
-            } 
-        }
-         //We subtract the length by 2 since each emoji is a surrogate pair of length 2
-        //@@@ this is buggy since some emojis are two characters long and some are 1
-        outputText.innerHTML = outputText.innerHTML.substring(0, outputText.innerHTML.length - 2);
-    } else if (thisKey == " ") {
-        console.log("space pressed");
-//        outputText.innerHTML += "&nbsp;";    
-    } else {
-        outputText.innerHTML += thisKey;
+//    console.log(inputBox.value);
+    var currentText = inputBox.value;
+    currentText = currentText.toLowerCase();
+    var currentCode;
+    outputString = "";
+    for (let i = 0; i < currentText.length; i++) {
+        var updatedValue = textToEmoji(currentText.charAt(i).charCodeAt(0));
+        if (updatedValue != -1) {
+            outputString = outputString.concat(String.fromCodePoint(updatedValue));
+        }  
     }
-}
-
-//! Backspace and Space do not work as expected.
-function updateSpacing(e, current) {
+    outputText.innerHTML = outputString;
     
+    //Leftover character (the one that fired the event) for "real-time" feel
+    var thisKey = e.key;
+    currentCode = thisKey.charCodeAt(0);
+    if ((currentCode - 97) >= 0 && (currentCode - 97) <= 26) {
+        var updatedValue = textToEmoji(currentCode);
+        if (updatedValue != -1) {
+            outputText.innerHTML = outputText.innerHTML.concat(String.fromCodePoint(updatedValue));
+        } 
+    } 
+    
+    //Backspace
+    else if (thisKey == "Backspace") {
+        currentText = currentText.substr(0,outputString.length-1);
+        //retranslate the outputString
+        outputString = "";
+        for (let i = 0; i < currentText.length; i++) {
+            var updatedValue = textToEmoji(currentText.charAt(i).charCodeAt(0));
+            if (updatedValue != -1) {
+                outputString = outputString.concat(String.fromCodePoint(updatedValue));
+            }  
+        }
+        outputText.innerHTML = outputString;
+    }
+//        var currentText = outputText.innerText;
+////        console.log("backspace pressed!");
+////        console.log("currentText: " + currentText);
+////        console.log("currrentText.length: " + currentText.length);
+//        if (currentText.length > 0) {
+//            var lastChar = currentText.codePointAt(currentText.length - 1);
+////            console.log("last code point: " + lastChar);
+////            console.log("charAt: " + currentText.charAt(currentText.length-1));
+//        }
+////        var lastChar = currentText.charAt(currentText.length - 1);
+////        var lengthOfLastChar = fancyCount2(lastChar);
+////        console.log("lastChar " + lastChar);
+////        console.log("length of the char " + lengthOfLastChar);
+////        if (currentText.length > 4) {
+////            if (currentText.substring(currentText.length-6) == "&nbsp;") {
+////                outputText.innerHTML = outputText.innerHTML.substring(0, outputText.innerHTML.length - 7);
+////            } 
+////        }
+//         //We subtract the length by 2 since each emoji is a surrogate pair of length 2
+//        //@@@ this is buggy since some emojis are two characters long and some are 1
+//        outputText.innerHTML = outputText.innerHTML.substring(0, outputText.innerHTML.length - 2);
+//    } else if (thisKey == " ") {
+//        console.log("space pressed");
+////        outputText.innerHTML += "&nbsp;";    
+//    } else {
+//        outputText.innerHTML += thisKey;
+//    }
 }
 
 function deleteText() {
@@ -128,7 +149,6 @@ function copyText() {
 }
 
 inputBox.addEventListener("keydown", function(e) { updateInput(e, this);}, false);
-inputBox.addEventListener("keydown", function(e) {updateSpacing(e, this);}, false);
 
 clearButton.addEventListener("click", deleteText, false);
 copyButton.addEventListener("click", copyText, false);
