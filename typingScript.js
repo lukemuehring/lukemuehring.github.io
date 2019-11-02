a7223a386fec91c3e371282af95c885b7ddbc665
lukemuehring <muehring.luke@gmail.com>
2019-11-02 16:14:26 -0400

    added backspace and space functionality, compound words but buggy

--- typingScript.js
+++ typingScript.js
@@ -1,6 +1,5 @@
-const testWrapper = document.querySelector(".test-wrapper");
-const inputBox = document.querySelector("#test-area");
-const outputText = document.querySelector("#output-text p");
+const inputBox = document.querySelector("#test-area"); //input textarea
+const outputText = document.querySelector("#output-text pre"); //output paragraph
 const clearButton = document.querySelector("#clearButton");
 const copyButton = document.querySelector("#copyButton");
 
@@ -32,25 +31,12 @@ var emojiArr = [
             0x1F4B9, //Y
             0x1F4A4, //Z
            ];
+
 var outputString;
 
-function fancyCount2(str){
-  const joiner = "\u{200D}";
-  const split = str.split(joiner);
-  let count = 0;
-
-  for(const s of split){
-    //removing the variation selectors
-    const num = Array.from(s.split(/[\ufe00-\ufe0f]/).join("")).length;
-    count += num;
-  }
-
-  //assuming the joiners are used appropriately
-  return count / split.length;
-}
-
-//a function that takes in the unicode code point of a character, and returns the emoji at the corresponding array index if it's within bounds (a-z). If not within bounds, it returns -1.
+//a function that takes in the unicode code point of a character, and returns the emoji at the corresponding emojiArr index if it's within bounds (a-z). If not within bounds, it returns -1.
 function textToEmoji(currentCode) {
+    //a-z
     if ((currentCode - 97) >= 0 && (currentCode - 97) <= 26) {
         return emojiArr[currentCode - 97];
     } else {
@@ -58,84 +44,84 @@ function textToEmoji(currentCode) {
     }
 }
 
-//! Need to figure out how to replace keypress with keydown.
 function updateInput(e, current) {
     //e is the event object
     //current is the DOM element that the eventObject is attached to, in this case the input box.
     
     //@@@ Take into account multiple word special cases like OK, 1234, ATM, GO and UP.
-    //@@@ Be able to delete spaces
-    //@@@ Alternate emojis by using a modifier key like SHIFT
-//    console.log(inputBox.value);
-    var currentText = inputBox.value;
+
+    var currentText = inputBox.value; //the initial value of the textarea. Always one character behind...
+    
+    //Because of the one character behind behavior, we have to add on the key that triggered the event
+    //First we make sure the key is a-z:
+    var currentCode = e.key.charCodeAt(0);
+    var updatedValue = textToEmoji(currentCode);
+    if (updatedValue != -1) {
+        currentText += e.key;
+    } else {
+        if (e.key == "Backspace") {
+            currentText = currentText.substr(0, currentText.length - 1);
+        }
+    }
+    
     currentText = currentText.toLowerCase();
-    var currentCode;
     outputString = "";
-    for (let i = 0; i < currentText.length; i++) {
+    
+    //Translating to Emoji
+    let i = 0;
+    while (i < currentText.length) {
         var updatedValue = textToEmoji(currentText.charAt(i).charCodeAt(0));
         if (updatedValue != -1) {
             outputString = outputString.concat(String.fromCodePoint(updatedValue));
-        }  
-    }
-    outputText.innerHTML = outputString;
-    
-    //Leftover character (the one that fired the event) for "real-time" feel
-    var thisKey = e.key;
-    currentCode = thisKey.charCodeAt(0);
-    if ((currentCode - 97) >= 0 && (currentCode - 97) <= 26) {
-        var updatedValue = textToEmoji(currentCode);
-        if (updatedValue != -1) {
-            outputText.innerHTML = outputText.innerHTML.concat(String.fromCodePoint(updatedValue));
-        } 
-    } 
-    
-    //Backspace
-    else if (thisKey == "Backspace") {
-        currentText = currentText.substr(0,outputString.length-1);
-        //retranslate the outputString
-        outputString = "";
-        for (let i = 0; i < currentText.length; i++) {
-            var updatedValue = textToEmoji(currentText.charAt(i).charCodeAt(0));
-            if (updatedValue != -1) {
-                outputString = outputString.concat(String.fromCodePoint(updatedValue));
-            }  
+        } else {
+            outputString += currentText.charAt(i);
         }
-        outputText.innerHTML = outputString;
+        //Check for compound words 
+        //Compound words of length: 3
+        if (i < currentText.length - 2) {
+            if (currentText.charAt(i) == 't' && currentText.charAt(i + 1) == 'o'&& currentText.charAt(i + 2) == 'p') {
+                outputString = outputString.substr(0,outputString.length - 1); //Delete 1st char
+                i = i + 2; //Skip the second and third char
+                outputString = outputString.concat(String.fromCodePoint(0x1F51D));
+            }
+        }
+        //Compound words of length: 2
+        if (i < currentText.length - 1) {
+            if (currentText.charAt(i) == 'w' && currentText.charAt(i + 1) == 'c') {
+                outputString = outputString.substr(0,outputString.length - 1); //Delete 1st char
+                i++; //Skip the second char
+                outputString = outputString.concat(String.fromCodePoint(0x1F6BE));
+            } else if (currentText.charAt(i) == 'o' && currentText.charAt(i + 1) == 'n') {
+                outputString = outputString.substr(0,outputString.length - 2); //??? buggy idk why its 2
+                i++; //Skip the second char
+                outputString = outputString.concat(String.fromCodePoint(0x1F51B));
+            } else if (currentText.charAt(i) == 'o' && currentText.charAt(i + 1) == 'k') {
+                outputString = outputString.substr(0,outputString.length - 2); //??? buggy idk why its 2
+                i++; //Skip the second char
+                outputString = outputString.concat(String.fromCodePoint(0x1f197));
+            } else if (currentText.charAt(i) == 'n' && currentText.charAt(i + 1) == 'g') {
+                outputString = outputString.substr(0,outputString.length - 1); 
+                i++; //Skip the second char
+                outputString = outputString.concat(String.fromCodePoint(0x1f196));
+            } else if (currentText.charAt(i) == 'a' && currentText.charAt(i + 1) == 'b') {
+                outputString = outputString.substr(0,outputString.length - 2); //??? buggy idk why its 2
+                i++; //Skip the second char
+                outputString = outputString.concat(String.fromCodePoint(0x1f18e));
+            }
+        }
+        i++;
     }
-//        var currentText = outputText.innerText;
-////        console.log("backspace pressed!");
-////        console.log("currentText: " + currentText);
-////        console.log("currrentText.length: " + currentText.length);
-//        if (currentText.length > 0) {
-//            var lastChar = currentText.codePointAt(currentText.length - 1);
-////            console.log("last code point: " + lastChar);
-////            console.log("charAt: " + currentText.charAt(currentText.length-1));
-//        }
-////        var lastChar = currentText.charAt(currentText.length - 1);
-////        var lengthOfLastChar = fancyCount2(lastChar);
-////        console.log("lastChar " + lastChar);
-////        console.log("length of the char " + lengthOfLastChar);
-////        if (currentText.length > 4) {
-////            if (currentText.substring(currentText.length-6) == "&nbsp;") {
-////                outputText.innerHTML = outputText.innerHTML.substring(0, outputText.innerHTML.length - 7);
-////            } 
-////        }
-//         //We subtract the length by 2 since each emoji is a surrogate pair of length 2
-//        //@@@ this is buggy since some emojis are two characters long and some are 1
-//        outputText.innerHTML = outputText.innerHTML.substring(0, outputText.innerHTML.length - 2);
-//    } else if (thisKey == " ") {
-//        console.log("space pressed");
-////        outputText.innerHTML += "&nbsp;";    
-//    } else {
-//        outputText.innerHTML += thisKey;
-//    }
+    console.log(outputString);
+    outputText.innerHTML = outputString;
 }
 
 function deleteText() {
     outputText.innerHTML = "";
     //HOW TO MAKE INNER TEXT DELETE AS WELL?
-    inputBox.innerText = "";
-    
+//    <textarea id="test-area" name="textarea" rows="6" placeholder="Type here!"></textarea>  
+//    const el = document.createElement('textarea');
+//    el.id = "#test-area";
+//    inputBox.parentNode.replaceChild(el, inputBox);
 }
 
 function copyText() {
