5ac3c20292a29229a52d67dbaa26af7b5d0903f3
lukemuehring <muehring.luke@gmail.com>
2023-09-27 12:52:30 -0700

    Added sprite animation
    sprite image animation, flipping when walking, and preloading images.

--- index.js
+++ index.js
@@ -1,28 +1,77 @@
+// Preloading Images
+this.addEventListener("DOMContentLoaded", preloadImages, true);
+
+var loadedImages = 0;
+var imageArray = new Array(
+    "./images/player/jump_0.png",
+    "./images/player/stand1_0.png",
+    "./images/player/stand1_1.png",
+    "./images/player/stand1_2.png",
+    "./images/player/walk1_0.png",
+    "./images/player/walk1_1.png",
+    "./images/player/walk1_2.png",
+    "./images/player/walk1_3.png"
+);
+
+function preloadImages(e) {
+    for (var i = 0; i < imageArray.length; i++) {
+        var tempImage = new Image();
+         
+        tempImage.addEventListener("load", trackProgress, true);
+        tempImage.src = imageArray[i];
+    }
+}
+
+function trackProgress() {
+    loadedImages++;
+     
+    if (loadedImages == imageArray.length) {
+        window.requestAnimationFrame(loop);
+    }
+}
+
 let canvas = document.querySelector('canvas')
 
-var canvasWidth = window.innerWidth;
-var canvasHeight = window.innerHeight;
+var canvas_width = window.innerWidth;
+var canvas_height = window.innerHeight;
 
 const c = canvas.getContext("2d");
 
-c.canvas.width = canvasWidth;
-c.canvas.height = canvasHeight;
+c.canvas.width = canvas_width;
+c.canvas.height = canvas_height;
 
+// Game Variables
 const JUMP_HEIGHT = 20; 
 const GRAVITY = 1.5;
+const ANIMATION_TIME_BUFFER = 30;
+
+const STATES = {
+    Idle: 0,
+    Jumping: 1,
+    Walking: 2
+}
+
+var frame_count = 0;
 
 const player = {
     height: 32,
     width: 32,
-    isJumping: true,
+    image: new Image(),
+    state: STATES.Jumping,
+    idle_sprite_frame: 0,
+    idle_sprite_frame_is_increasing: true,
+    walk_sprite_frame: 0,
+    is_going_to_the_right: true,
     x: 0,
-    xVelocity: 0,
+    x_velocity: 0,
     y: 0,
-    yVelocity: 0
+    y_velocity: 0
 };
 
+player.image.src = "./images/player/stand1_0.png";
+
 const floor = {
-    height: canvasHeight - 100,
+    height: canvas_height - 100,
     color: "white"
 }
 
@@ -49,56 +98,123 @@ const controller = {
 }
 
 const loop = function() {
-    if (controller.up && !player.isJumping) {
-        player.yVelocity -= JUMP_HEIGHT;
-        player.isJumping = true;
+
+    // Controller Input
+    if (controller.up && !player.state == STATES.Jumping) {
+        player.y_velocity -= JUMP_HEIGHT;
+        player.state = STATES.Jumping;
     }
 
     if (controller.left) {
-        player.xVelocity -= .5;
+        player.x_velocity -= .5;
     }
 
     if (controller.right) {
-        player.xVelocity += .5;
+        player.x_velocity += .5;
     }
 
     // Gravity and Friction
-    player.yVelocity += GRAVITY;
-    player.x += player.xVelocity;
-    player.y += player.yVelocity;
+    player.y_velocity += GRAVITY;
+    player.x += player.x_velocity;
+    player.y += player.y_velocity;
 
-    player.xVelocity *= .9;
-    player.yVelocity += .9;
+    player.x_velocity *= .9;
 
-    // Collision with floor
+    // If the xVelocity is close enough to 0, we set it to 0 for animation purposes.
+    if (player.x_velocity <= 0.2 && player.x_velocity >= -0.2) {
+        player.x_velocity = 0;
+    }
+    player.y_velocity += .9;
+
+    // Collision with floor and Walking + Idle animations
     if (player.y > floor.height - player.height - JUMP_HEIGHT) {
-        player.isJumping = false;
+        if ((controller.left || controller.right) && !controller.up) {
+            player.state = STATES.Walking;
+            player.is_going_to_the_right = controller.right;
+        } else {
+            player.state = STATES.Idle;
+        }
         player.y = floor.height - player.height - JUMP_HEIGHT;
-        player.yVelocity = 0;
+        player.y_velocity = 0;
     }
 
     // Background Fill
-    c.fillStyle = "red";
-    c.fillRect(0,0, canvasWidth, canvasHeight);
+    c.fillStyle = "pink";
+    c.fillRect(0,0, canvas_width, canvas_height);
 
-    // Player Fill
+    // Player Draw
+    // console.log("natural height height" + player.image.naturalHeight);
+    switch(player.state) {
+        case STATES.Jumping:
+            //code
+            player.image.src = "./images/player/jump_0.png";
+            break;
+        case STATES.Walking:
+            if (frame_count % (ANIMATION_TIME_BUFFER / 5) == 0) {
+                player.walk_sprite_frame = (player.walk_sprite_frame + 1) % 4;
+                console.log("walking animation frame change to: " + player.walk_sprite_frame);
+            }
+            player.image.src = "./images/player/walk1_" + player.walk_sprite_frame + ".png";
+
+            //code
+            break;
+        case STATES.Idle:
+            // idle
+            console.log(player.x_velocity);
+            if (frame_count % ANIMATION_TIME_BUFFER == 0 && player.x_velocity == 0) {
+                if (player.idle_sprite_frame == 2)
+                {
+                    idle_sprite_frame_is_increasing = false;
+                } else if (player.idle_sprite_frame == 0) {
+                    idle_sprite_frame_is_increasing = true;
+                }
+
+                if (idle_sprite_frame_is_increasing) {
+                    player.idle_sprite_frame = (player.idle_sprite_frame + 1);
+                } else {
+                    player.idle_sprite_frame = (player.idle_sprite_frame - 1);
+                }
+                player.image.src = "./images/player/stand1_" + player.idle_sprite_frame + ".png";
+            }
+            break;
+    }
+
+    if (player.is_going_to_the_right) {
+        drawFlippedImage(c, player.image, player.x, player.y - player.image.naturalHeight);
+    } else {
+        c.drawImage(player.image, player.x, player.y - player.image.naturalHeight);
+    }
+
+    // To be deprecated
     c.fillStyle = "blue";
     c.beginPath();
     c.rect(player.x, player.y, player.width, player.height);
     c.fill();
 
     // Floor Fill
-    c.strokeStyle = "#2E2532";
+    c.strokeStyle = floor.color;
     c.lineWidth = 30;
     c.beginPath();
     c.moveTo(0, floor.height);
-    c.lineTo(canvasWidth, floor.height);
+    c.lineTo(canvas_width, floor.height);
     c.stroke();
 
     // Animation
     window.requestAnimationFrame(loop);
+    frame_count++;
+
+    console.log("player state:" + player.state);
+};
+
+function drawFlippedImage(context, image, x, y) {
+    context.save();
+    context.translate(x+player.image.width/2,0);
+    context.scale(-1,1);
+    context.translate(-(x+player.image.width/2),0);
+    context.drawImage(image, x, y);
+    context.restore();
 };
 
 window.addEventListener("keydown", controller.keyListener)
 window.addEventListener("keyup", controller.keyListener);
-window.requestAnimationFrame(loop);
+// window.requestAnimationFrame(loop);
